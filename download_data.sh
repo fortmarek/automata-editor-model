77577ac859fac2aa3ed1018021b156218afdbd13
fortmare <fortmare@fit.cvut.cz>
2021-04-20 09:34:55 +0200

    Commit data

--- download_data.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p data/bitmaps
-mkdir -p data/strokes
-mkdir -p data/bitmaps
-mkdir -p data/sframes
-curl https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/triangle.npy > data/bitmaps/triangle.npy
-curl https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/circle.npy > data/bitmaps/circle.npy
-curl https://storage.googleapis.com/quickdraw_dataset/full/raw/triangle.ndjson > data/strokes/triangle.ndjson
-curl https://storage.googleapis.com/quickdraw_dataset/full/raw/circle.ndjson > data/strokes/circle.ndjson
